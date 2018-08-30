9fc89e8dae9f57273e76cb506c9ea09240a97d00
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2018-08-30 14:04:40 +0530

    about page

--- app/src/main/java/com/arnab/skinsense/AboutUc.java
+++ app/src/main/java/com/arnab/skinsense/AboutUc.java
@@ -7,13 +7,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by User on 20-03-2018.
  */
 
-public class AboutUc extends android.support.v4.app.Fragment
+public class AboutUc extends android.support.v4.app.Fragment implements View.OnClickListener
 {
+    ImageView arnab, tanay, ritvik, rajnish;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,5 +25,18 @@ public class AboutUc extends android.support.v4.app.Fragment
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        arnab = view.findViewById(R.id.arnab);
+        tanay = view.findViewById(R.id.tanay);
+        ritvik = view.findViewById(R.id.ritvik);
+        rajnish = view.findViewById(R.id.rajnish);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.arnab:
+
+        }
     }
 }
